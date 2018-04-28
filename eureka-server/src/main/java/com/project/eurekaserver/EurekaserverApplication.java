d2274ebbf462e9b908cb7eb1139ab8104c62a9b7
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-28 11:27:36 +0300

    init eureka server

--- eureka-server/src/main/java/com/project/eurekaserver/EurekaserverApplication.java
+++ eureka-server/src/main/java/com/project/eurekaserver/EurekaserverApplication.java
@@ -1,9 +1,15 @@
 package com.project.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableEurekaServer
+@EnableAutoConfiguration
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
