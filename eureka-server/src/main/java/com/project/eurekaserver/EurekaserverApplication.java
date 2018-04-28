ea0f310f3f01b0079ffd4ebd161b97dfdf6c56ec
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-28 11:24:31 +0300

    add to eureka server

---
+++ eureka-server/src/main/java/com/project/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,12 @@
+package com.project.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+}
