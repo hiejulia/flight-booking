44d977d10c3e1ffd17cad809dd1f69a96ee58d9d
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-28 14:00:59 +0300

    fix dependencies list

--- user-service/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ user-service/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -3,8 +3,37 @@ package com.project.flightbooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+@ComponentScan({"com.packtpub.mmj.user", "com.packtpub.mmj.common"})
+
 public class FlightbookingApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(FlightbookingApplication.class);
+
+	@Value("${app.rabbitmq.host:localhost}")
+	String rabbitMqHost;
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		LOG.info("Create RabbitMqCF for host: {}", rabbitMqHost);
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
+		return connectionFactory.getRabbitConnectionFactory();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightbookingApplication.class, args);
