f85c2a0d9a80ce253ab0c1bf1caffca99c7f1403
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-28 18:37:01 +0300

    fix turbine dependency

--- turbine-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ turbine-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableTurbineStream
+@EnableEurekaClient
 public class FlightbookingApplication {
 
 	public static void main(String[] args) {
