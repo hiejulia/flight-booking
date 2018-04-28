b028dde19fc62f7bd8cb2b3503463139102ff20b
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-28 18:26:09 +0300

    update to common

---
+++ common/src/main/java/com/project/flightbooking/MDCHystrixConcurrencyStrategy.java
@@ -0,0 +1,13 @@
+package com.project.flightbooking;
+
+
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import java.util.concurrent.Callable;
+
+public class MDCHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
+
+    @Override
+    public <T> Callable<T> wrapCallable(Callable<T> callable) {
+        return new MDCConcurrentCallable<>(callable);
+    }
+}
