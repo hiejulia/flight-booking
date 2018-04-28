60f4a88659b27fbeb74d1d26b128ddac0622ad69
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-28 18:29:59 +0300

    update to hystrix dashboard

--- dashboard-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ dashboard-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -2,9 +2,26 @@ package com.project.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class FlightbookingApplication {
+@Controller
+@EnableHystrixDashboard
+public class FlightbookingApplication extends SpringBootServletInitializer {
+
+		@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FlightbookingApplication.class).web(true);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightbookingApplication.class, args);
