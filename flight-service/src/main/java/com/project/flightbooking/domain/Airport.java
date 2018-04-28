4a74a54aa6775c9afc40dceb19d4f6c0c3410837
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-04-28 19:13:12 +0300

    init entity

---
+++ flight-service/src/main/java/com/project/flightbooking/domain/Airport.java
@@ -0,0 +1,44 @@
+package com.project.flightbooking.domain;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Airport extends BaseEntity<String> {
+
+    private List<Flight> flights = new ArrayList<>();
+
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public Airport(String name, String id, String city, List<Flight> flights) {
+        super(id, name);
+        this.city = city;
+        this.flights = flights;
+    }
+
+
+    public void setTables(List<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("{id: %s, name: %s, address: %s, tables: %s}", this.getId(),
+                this.getName(), this.getCity(), this.getFlights());
+    }
+}
---
+++ flight-service/src/main/java/com/project/flightbooking/domain/Flight.java
@@ -0,0 +1,34 @@
+package com.project.flightbooking.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.math.BigInteger;
+
+
+public class Flight extends BaseEntity<BigInteger> {
+    // Flight : capacity
+
+    private int capacity;
+
+    public Flight(@JsonProperty("name") String name, @JsonProperty("id") BigInteger id, @JsonProperty("capacity") int capacity) {
+        super(id, name);
+        this.capacity = capacity;
+    }
+
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("{id: %s, name: %s, capacity: %s}",
+                this.getId(), this.getName(), this.getCapacity());
+    }
+
+}
