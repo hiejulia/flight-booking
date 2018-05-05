9ddc53ef99dd02fa597fc5d298540522e80b505e
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-05-05 13:37:34 +0300

    update

--- notification-service/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ notification-service/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -2,8 +2,10 @@ package com.project.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // Enable scheduling for notification service
 public class FlightbookingApplication {
 
 	public static void main(String[] args) {
--- notification-service/src/main/java/com/project/flightbooking/NotificationScheduler.java
+++ notification-service/src/main/java/com/project/flightbooking/NotificationScheduler.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import com.project.flightbooking.dto.Passenger;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -20,43 +21,34 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 
-
 @Component
 public class NotificationScheduler {
 
-    Map<String,String> hobbies;
-
-    @PostConstruct
-    public void addNotificationsToHobbies() {
-        hobbies = new HashMap<>();
-        hobbies.put("music","There is a rock concert going on");
-        hobbies.put("movies","There is an action film coming this weekend");
-        hobbies.put("football","Watch Manchester vs Arsenal live");
-    }
-
     @Autowired
-    private RestTemplate restTemplate;
+    private RestTemplate restTemplate; // Rest Template
 
     private static final Logger log = LoggerFactory.getLogger(NotificationScheduler.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     private static final String FIREBASE_SERVER_KEY = "AAAAoeDtrfQ:APA91bHXsvdLTjdCjEIPBPlBGhxjb3uv6URK2f8s8TjP9A1Kpwmm1RkbBKU_xrZWN6olceJEtmP9JMhmB2-yMMznSK0QhlOWoYtetg7j3-lmDdE_8Xkc1FCyp2uzOaiZGPBTwE55JOML";
     private static final String FIREBASE_API_URL = "https://fcm.googleapis.com/fcm/send";
 
+    // Report current time :
     @Scheduled(fixedRate = 50000)
     public void reportCurrentTime() {
-        log.info("Pushing notifications now at " + dateFormat.format(new Date()));
-        User[] usersArray = restTemplate.getForObject("http://172.31.84.105:8080/uaa/findall", User[].class);
-        send(usersArray);
+
+        Passenger[] usersArray = restTemplate.getForObject("http://172.31.84.105:8080/uaa/findall", User[].class); // find all user
+        send(usersArray);// send
     }
 
+    // send report
+    public void send(Passenger[] usersArray) {
 
-    public void send(User[] usersArray) {
-
-        for (User user : usersArray) {
+        for (Passenger user : usersArray) {
 
             JSONObject body = new JSONObject();
 
             try {
+                // each passenger
 
                 body.put("to", "/topics/"+user.getHobby());
                 body.put("priority", "high");
@@ -77,7 +69,7 @@ public class NotificationScheduler {
             headers.set("Content-Type", "application/json");
 
             HttpEntity<String> request = new HttpEntity<>(body.toString(),headers);
-            System.out.println(body.toString());
+            // post
             restTemplate.postForObject(FIREBASE_API_URL, request, String.class);
         }
     }
