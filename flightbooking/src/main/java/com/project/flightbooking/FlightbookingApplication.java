548526837b1d4bef5900d4de3cc98f32c7ca25c5
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-05-09 21:57:46 +0300

    update to config server

--- flightbooking/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ flightbooking/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -2,8 +2,11 @@ package com.project.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class FlightbookingApplication {
 
 	public static void main(String[] args) {
