14be2fc821c9a971397768e67ca5356688bb7a79
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-05-13 12:26:14 +0300

    update to spring cloud data flow

---
+++ data-flow-server/README.md
@@ -0,0 +1,7 @@
+## Spring Cloud Data Flow Server 
+Spring Cloud DataFlow server for Stream and Task / Batch applications
+
+### Development 
+### Reference 
++ DataFlow Server 
++ Spring Cloud Stream App Starters  
--- data-flow-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ data-flow-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -2,8 +2,10 @@ package com.project.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
 
 @SpringBootApplication
+@EnableDataFlowServer // Enable data flow server 
 public class FlightbookingApplication {
 
 	public static void main(String[] args) {
