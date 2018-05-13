9359f2e80783ff81141fefe0df5dbd81459a15b9
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-05-14 00:02:26 +0300

    update the project

--- cloud-skipper-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ cloud-skipper-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -2,8 +2,15 @@ package com.project.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.cloud.skipper.server.EnableSkipperServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		SessionAutoConfiguration.class
+})
+@EnableSkipperServer
 public class FlightbookingApplication {
 
 	public static void main(String[] args) {
