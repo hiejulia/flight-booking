3482bff4829e2c4fc5c143711d7a6e1b53ed8c37
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-05-13 12:28:03 +0300

    init cloud skipper server

--- cloud-skipper-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ cloud-skipper-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -2,10 +2,8 @@ package com.project.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
 
 @SpringBootApplication
-@EnableDataFlowServer // Enable data flow server
 public class FlightbookingApplication {
 
 	public static void main(String[] args) {
