3692adb4e441daf7d9e2959b9d6cb790541a1d2f
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-05-20 13:52:14 +0300

    init auth server

---
+++ auth-server/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -0,0 +1,12 @@
+package com.project.flightbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightbookingApplication.class, args);
+	}
+}
