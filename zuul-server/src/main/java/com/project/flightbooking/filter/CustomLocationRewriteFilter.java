eeb7d94c3c7ac89337ed7956b7d64566591bbfcf
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-08-05 22:46:26 +0300

    update to api gateway

---
+++ zuul-server/src/main/java/com/project/flightbooking/filter/CustomLocationRewriteFilter.java
@@ -0,0 +1,19 @@
+package com.project.flightbooking.filter;
+
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Filter to re-write the {@code Location} header to point to the API gateway.
+ *
+ */
+public class CustomLocationRewriteFilter extends LocationRewriteFilter {
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        int statusCode = ctx.getResponseStatusCode();
+        return HttpStatus.valueOf(statusCode).is3xxRedirection() || HttpStatus.valueOf(statusCode) == HttpStatus.CREATED;
+    }
+}
