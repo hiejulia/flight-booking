02d5e9f9c51cc34c042d48e052456f75234e568c
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-08-15 11:36:21 +0300

    update config file for apache hive

---
+++ dataflow/flightbooking/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -0,0 +1,19 @@
+package com.project.flightbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+/**
+ * Add for getting passengers flight experience 
+ * 
+ * 
+ */
+@SpringBootApplication
+public class FlightbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightbookingApplication.class, args);
+	}
+}
