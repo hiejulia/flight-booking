2f69506b15324743e8d480b02022fa20068c8f8d
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-09-02 22:43:26 +0300

    create vagrant scripts

---
+++ vagrant-scripts/rabbitmq.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing RabbitMQ"
+
+apt-get -y install erlang-nox
+wget https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
+apt-key add rabbitmq-release-signing-key.asc
+echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
+apt-get update
+apt-get install -y rabbitmq-server
+
+rabbitmqctl add_user $1 $2
+rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
