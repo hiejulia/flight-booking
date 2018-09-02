7c5d9402811c528dc50e91ed72479778cb7a9d7f
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-09-02 18:26:25 +0300

    finish sleuth

--- sleuth/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ sleuth/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -4,9 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlightbookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightbookingApplication.class, args);
 	}
+
+
+	// Config TaskScheduler
+	@Bean
+	public TaskScheduler taskScheduler() {
+		return new ConcurrentTaskScheduler();
+	}
+
+	// Config RestTemplate
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
 }
---
+++ sleuth/src/main/java/com/project/flightbooking/controller/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package com.project.flightbooking.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    // Report current time
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
