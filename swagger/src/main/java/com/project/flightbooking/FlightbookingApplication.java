1fd178e25deb39a23e8602e203dbc8a09caf61ce
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-09-12 23:06:46 +0300

    finish swagger

--- swagger/src/main/java/com/project/flightbooking/FlightbookingApplication.java
+++ swagger/src/main/java/com/project/flightbooking/FlightbookingApplication.java
@@ -2,9 +2,25 @@ package com.project.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class FlightbookingApplication {
+@Configuration
+@EnableWebMvc
+public class FlightbookingApplication extends WebMvcConfigurerAdapter{
+
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+	@Bean
+	public SwaggerController swaggerController(final LoadBalancerClient loadBalancerClient, final DiscoveryClient discoveryClient) {
+		return new SwaggerController(loadBalancerClient, discoveryClient);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightbookingApplication.class, args);
