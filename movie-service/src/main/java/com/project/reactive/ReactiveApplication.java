5d1fae4f22097ee2db03225e104aa3136c5fd257
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-09-17 14:15:45 +0300

    init movie service

---
+++ movie-service/src/main/java/com/project/reactive/ReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.project.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApplication.class, args);
+	}
+}
